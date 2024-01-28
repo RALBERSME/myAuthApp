e608e9b2873398c8d73668ee4f72b1a792153d9f
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-01-28 19:30:53 +0100

    update Register page
    error message and loading message added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,50 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Register() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/server/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Register</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
 
         <input
@@ -17,15 +52,20 @@ export default function Register() {
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-2 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Register
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Register"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -34,6 +74,7 @@ export default function Register() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 }
